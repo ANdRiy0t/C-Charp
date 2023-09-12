4cb7aaaa9fc8b14c7dbec664ddd44bdce6153728
ANdRiy0t <114472084+ANdRiy0t@users.noreply.github.com>
2023-09-12 23:26:10 +0300

    Add files via upload

---
+++ TIcTAcToe Адаптив/TIcTAcToe Адаптив/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace TIcTAcToe_Адаптив
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			this.panel = new System.Windows.Forms.Panel();
+			this.Level1 = new System.Windows.Forms.RadioButton();
+			this.Level2 = new System.Windows.Forms.RadioButton();
+			this.Level3 = new System.Windows.Forms.RadioButton();
+			this.TIMER = new System.Windows.Forms.Timer(this.components);
+			this.panel.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// panel
+			// 
+			this.panel.BackColor = System.Drawing.SystemColors.ControlLight;
+			this.panel.Controls.Add(this.Level3);
+			this.panel.Controls.Add(this.Level2);
+			this.panel.Controls.Add(this.Level1);
+			this.panel.Location = new System.Drawing.Point(637, 114);
+			this.panel.Name = "panel";
+			this.panel.Size = new System.Drawing.Size(122, 196);
+			this.panel.TabIndex = 0;
+			// 
+			// Level1
+			// 
+			this.Level1.AutoSize = true;
+			this.Level1.Location = new System.Drawing.Point(30, 15);
+			this.Level1.Name = "Level1";
+			this.Level1.Size = new System.Drawing.Size(71, 20);
+			this.Level1.TabIndex = 0;
+			this.Level1.TabStop = true;
+			this.Level1.Text = "Level 1";
+			this.Level1.UseVisualStyleBackColor = true;
+			this.Level1.CheckedChanged += new System.EventHandler(this.Level1_CheckedChanged);
+			// 
+			// Level2
+			// 
+			this.Level2.AutoSize = true;
+			this.Level2.Location = new System.Drawing.Point(30, 86);
+			this.Level2.Name = "Level2";
+			this.Level2.Size = new System.Drawing.Size(71, 20);
+			this.Level2.TabIndex = 1;
+			this.Level2.TabStop = true;
+			this.Level2.Text = "Level 2";
+			this.Level2.UseVisualStyleBackColor = true;
+			// 
+			// Level3
+			// 
+			this.Level3.AutoSize = true;
+			this.Level3.Location = new System.Drawing.Point(30, 158);
+			this.Level3.Name = "Level3";
+			this.Level3.Size = new System.Drawing.Size(71, 20);
+			this.Level3.TabIndex = 2;
+			this.Level3.TabStop = true;
+			this.Level3.Text = "Level 3";
+			this.Level3.UseVisualStyleBackColor = true;
+			// 
+			// TIMER
+			// 
+			this.TIMER.Tick += new System.EventHandler(this.TIMER_Tick);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.panel);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.panel.ResumeLayout(false);
+			this.panel.PerformLayout();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Panel panel;
+		private System.Windows.Forms.RadioButton Level3;
+		private System.Windows.Forms.RadioButton Level2;
+		private System.Windows.Forms.RadioButton Level1;
+		private System.Windows.Forms.Timer TIMER;
+	}
+}
+
---
+++ TIcTAcToe Адаптив/TIcTAcToe Адаптив/Form1.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TIcTAcToe_Адаптив
+{
+	public partial class Form1 : Form
+	{
+		static int N = 3;
+		// це поле яке показує скільки обьєктів має бути в ряд щоб виграти
+		static int WinIN = 3;
+		// Масив карти
+		char[,] dataMAP = new char[N, N];
+		List<PictureBox> pictureBoxes = new List<PictureBox>();
+
+		char Player = 'X', BOT = 'O';
+		static int STARTposX = 10, STARTposY = 10;
+		static sbyte SIZE = 50;
+
+		Random rand = new Random();
+
+		// Масив в якому галочки ставлятся якщо для прикладу є 3 в ряд а WinIN = 3
+		byte[] ChekingForWin = new byte[WinIN];
+
+		byte x = 0, y = 0;
+
+
+		string HID = "Player";
+
+		public Form1()
+		{
+			InitializeComponent();
+			CreateMAP();
+
+
+			TIMER.Interval += 900;
+			TIMER.Tick += new EventHandler(TIMER_Tick);
+		}
+
+		public void CreateMAP()
+		{
+			for(int i = 0; i < N; i++)
+			{
+				for(int b= 0; b < N; b++)
+				{
+					PictureBox pic = new PictureBox();
+
+					pic.Name = i.ToString()+ "_" + b.ToString(); 
+					pic.Size = new Size(SIZE, SIZE);
+					pic.Location = new Point(STARTposX, STARTposY);
+					pic.BackColor = Color.LightGray;
+					pic.Click += new EventHandler(OnClick);
+
+					pictureBoxes.Add(pic);
+					// Завдяки Controls додаєтся на екран цей PicBox
+					this.Controls.Add(pic);
+					// зразу змінюємо наступні дані для локації наступного picbox
+					STARTposX += SIZE + 5;
+				}
+				STARTposX = 10;
+				STARTposY += SIZE + 5; 
+			}
+
+		}
+
+		private void OnClick(object sender, EventArgs e)
+		{
+			PictureBox pic = sender as PictureBox;
+
+			if (HID != "BOT" && pic.BackColor != Color.Green && pic.BackColor != Color.Red)
+			{
+				TakeLocation(pic, ref x, ref y);
+
+				dataMAP[x, y] = Player;
+				pictureBoxes[(x - 1) * N + y + N].BackColor = Color.Green;
+				HID = "BOT";
+				TIMER.Start();
+			}
+		}
+
+		private void Level1_CheckedChanged(object sender, EventArgs e)
+		{
+			Level1_RANDOM();
+		}
+
+		private void TIMER_Tick(object sender, EventArgs e)
+		{
+			TIMER.Stop();
+
+			Level1_RANDOM();
+		}
+
+		// Даний метод визначає Позицїю квадратика на який нажали, Воно працює елементарно назва любого PictureBox Складаєтся з 2 цифр які задані  нижнім піжкресленням
+		// В методі записуются дані в String з перевіркою чи цей символ не є '_' потім парсить дані з string в byte
+		private void TakeLocation(PictureBox pic, ref byte x, ref byte y)
+		{
+			char[] arr = pic.Name.ToCharArray();
+			
+			string result1 = string.Empty, result2 = string.Empty;
+
+			bool HelpBool = false;
+			foreach (char helpChar in arr)
+			{
+				_ = helpChar == '_' ? HelpBool = true : HelpBool;
+				switch (HelpBool) {
+					case false:
+						result1 += helpChar.ToString();
+						break;
+					case true:
+						_=helpChar != '_' ? result2 += helpChar.ToString() : helpChar.ToString();
+						break;				
+				
+				}
+			}
+
+				x = Byte.Parse(result1);
+				y = Byte.Parse(result2);
+		}
+
+		private void ChechWho_WinGame(int CountCubeForWin, char PlayerOrBot)
+		{
+			for(int i = 0; i < N; i++)
+			{
+				for(int b=0; b < N; b++)
+				{
+					
+				}
+			}
+		}
+
+
+
+		//		___     __   _______           
+		//		-  -  -    -    |
+		//		---   -    -    |
+		//		-  -  -    -    |
+		//		---     --	    |
+
+
+
+		private void Level1_RANDOM()
+		{
+			if (HID != "Player")
+			{
+
+				int random = rand.Next(N * N);
+
+				if (pictureBoxes[random].BackColor == Color.LightGray)
+				{
+					TakeLocation(pictureBoxes[random], ref x, ref y);
+					pictureBoxes[random].BackColor = Color.Red;
+					dataMAP[x, y] = BOT;
+					HID = "Player";
+					return;
+				}
+				else
+					//HID = "Player";
+				Level1_RANDOM();
+
+			}
+			//GC.Collect();
+		}
+	}
+}
